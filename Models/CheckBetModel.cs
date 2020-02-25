5773376e849f391deae1cca54a091b712300ac9c
narek.hayrapetyan <narvladhairo@gmail.com>
2020-02-25 16:48:48 +0400

    chnages made

--- Models/CheckBetModel.cs
+++ Models/CheckBetModel.cs
@@ -3,7 +3,7 @@
 namespace TestPartnersPages.Models
 {
     public class CheckBetModel
-     {
+    {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
         //public string BetFrameName { get; set; }
         public string GameBetElement { get; set; }
@@ -13,6 +13,7 @@ namespace TestPartnersPages.Models
         public string MultipleBetAmountInputElement { get; set; }
         public string MultipleBetAmount { get; set; }
         public string WaitAfterBetElement { get; set; }
+        public string Helo { get; set; }
 
     }
 }
